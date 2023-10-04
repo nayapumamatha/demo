24b171156e761f712b15c710976025b3bef2bba7
nayapumamatha <145565877+nayapumamatha@users.noreply.github.com>
2023-10-04 15:44:04 +0530

    Add files via upload

---
+++ bar.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+plt.bar([1,3,5,7,9],[5,2,7,8,2],label="data 1")
+plt.legend()
+plt.xlabel("x values ")
+plt.ylabel("y height ")
+plt.title("verticle bar chart")
+plt.show()
